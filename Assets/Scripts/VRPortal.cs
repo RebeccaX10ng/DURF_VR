a2fe8dc2b8b03fd8b044b63fa04e1da84a8caba5
RebeccaX10ng <jx2494@nyu.edu>
2024-07-04 21:04:35 +0800

    1

---
+++ Assets/Scripts/VRPortal.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class VRPortal : MonoBehaviour
+{
+    public Transform destination;
+    public float cooldownTime = 3f; // 冷却时间
+
+    private bool canTeleport = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && canTeleport)
+        {
+            Debug.Log("Player entered the portal");
+            TeleportPlayer(other.transform);
+            StartCoroutine(TeleportCooldown());
+        }
+    }
+
+    private void TeleportPlayer(Transform player)
+    {
+        player.position = destination.position;
+        player.rotation = destination.rotation;
+    }
+
+    private IEnumerator TeleportCooldown()
+    {
+        canTeleport = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canTeleport = true;
+    }
+}
