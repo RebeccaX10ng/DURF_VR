909654ce5796b00252465b80ed865b58db60720c
abbre <sc9771@nyu.edu>
2024-07-11 09:51:17 +0800

    场景

--- Assets/Scripts/VRCameraController.cs
+++ Assets/Scripts/VRCameraController.cs
@@ -1,28 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class VRCameraController : MonoBehaviour
+public class MirrorCamera : MonoBehaviour
 {
-    public Camera mainCamera;
-    public Camera vrCamera;
-
-    void Start()
-    {
-        SyncVRCameraWithMainCamera();
-    }
+    public Camera mainCamera; // 主相机
+    public Camera mirrorCamera; // 镜像相机
 
     void Update()
     {
-        SyncVRCameraWithMainCamera();
-    }
+        // 获取主相机的位置和旋转
+        Vector3 mainCamPosition = mainCamera.transform.position;
+        Quaternion mainCamRotation = mainCamera.transform.rotation;
 
-    private void SyncVRCameraWithMainCamera()
-    {
-        Vector3 mainCameraPosition = mainCamera.transform.position;
-        Vector3 vrCameraPosition = new Vector3(mainCameraPosition.x, mainCameraPosition.y, mainCameraPosition.z - 5f);
-        
-        vrCamera.transform.position = vrCameraPosition;
-        vrCamera.transform.rotation = mainCamera.transform.rotation;
+        // 计算镜像相机的位置
+        Vector3 mirrorCamPosition = new Vector3(-mainCamPosition.x - 1, mainCamPosition.y, mainCamPosition.z);
+
+        // 计算镜像相机的旋转
+        Quaternion mirrorCamRotation = new Quaternion(-mainCamRotation.x, mainCamRotation.y, mainCamRotation.z, -mainCamRotation.w);
+
+        // 应用到镜像相机
+        mirrorCamera.transform.position = mirrorCamPosition;
+        mirrorCamera.transform.rotation = mirrorCamRotation;
     }
 }
