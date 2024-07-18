4a60fe2033cc21ed138655a4b57a791536fe797a
abbre <sc9771@nyu.edu>
2024-07-18 17:13:42 +0800

    11

--- Assets/Scripts/VRCameraController.cs
+++ Assets/Scripts/VRCameraController.cs
@@ -7,15 +7,9 @@ public class MirrorCamera : MonoBehaviour
     void Update()
     {
         Vector3 mainCamPosition = mainCamera.transform.position;
-        Vector3 eulerRotation = mainCamera.transform.rotation.eulerAngles;
-        //eulerRotation.y = -eulerRotation.y;
         
         Vector3 mirrorCamPosition = new Vector3(-mainCamPosition.x - 1, mainCamPosition.y, mainCamPosition.z - 5);
         
-        //Quaternion mirrorCamRotation = new Quaternion.Euler(eulerRotation);
-        //Quaternion mirrorCamRotation = new Quaternion(0, -eulerRotation.y-180f, 0, 0);
-        
         mirrorCamera.transform.position = mirrorCamPosition;
-        //mirrorCamera.transform.rotation = mirrorCamRotation;
     }
 }
