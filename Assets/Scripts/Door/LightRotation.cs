a25e7f65c99972313158f5991aa8eada12259334
abbre <sc9771@nyu.edu>
2024-08-07 18:31:33 +0800

    机制写好了就差一个最后的判定+转场景

--- Assets/Scripts/Door/LightRotation.cs
+++ Assets/Scripts/Door/LightRotation.cs
@@ -1,48 +1,43 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LightRotation : MonoBehaviour
 {
-    public List<GameObject> rotationObjects;
-    public Light directionalLight;
-    public float ratio = 3f;
-    private Dictionary<GameObject, Vector3> previousRotations = new Dictionary<GameObject, Vector3>();
-    
-    void Start()
+    public float rotationSpeed = 10f;
+
+    private bool clockwise;
+    private bool isSelected;
+
+    public void IsClockwise()
     {
-        foreach (var obj in rotationObjects)
-        {
-            if (obj != null)
-            {
-                previousRotations[obj] = obj.transform.eulerAngles;
-            }
-        }
+        isSelected = true;
+        clockwise = true;
+    }
+
+    public void NotClockwise()
+    {
+        isSelected = true;
+        clockwise = false;
+    }
+
+    public void ExitSelect()
+    {
+        isSelected = false;
     }
 
     void Update()
     {
-        foreach (var obj in rotationObjects)
+        if (isSelected)
         {
-            if (obj != null)
+            if (clockwise)
             {
-                Vector3 currentRotation = obj.transform.eulerAngles;
-                Vector3 previousRotation = previousRotations[obj];
-                
-                // Calculate the delta rotation for both x and y axes
-                float deltaX = Mathf.DeltaAngle(previousRotation.x, currentRotation.x);
-                float deltaY = Mathf.DeltaAngle(previousRotation.y, currentRotation.y);
-                
-                // Apply the delta rotations to the directional light's x-axis rotation only
-                float totalDeltaX = (deltaX + deltaY) / ratio;
-                Vector3 lightRotation = directionalLight.transform.rotation.eulerAngles;
-                lightRotation.x += totalDeltaX;
-                
-                // Apply the new rotation to the directional light, preserving y and z axes
-                directionalLight.transform.rotation = Quaternion.Euler(lightRotation.x, lightRotation.y, lightRotation.z);
-
-                // Update the previous rotation
-                previousRotations[obj] = currentRotation;
+                float angle = rotationSpeed * Time.deltaTime;
+                transform.Rotate(Vector3.right, angle);
+            }
+            else
+            {
+                float angle = -rotationSpeed * Time.deltaTime;
+                transform.Rotate(Vector3.right, angle);
             }
         }
     }
---
+++ Assets/Scripts/Door/Rotator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Rotator : MonoBehaviour
+{
+    public enum RotationAxis { X, Y }
+    public RotationAxis rotationAxis = RotationAxis.X;
+    public float rotationSpeed = 15f; 
+    public bool rotateClockwise = true;
+
+    private bool isSelected = false;
+
+    public void Rotate()
+    {
+        isSelected = true;
+    }
+
+    public void StopRotate()
+    {
+        isSelected = false;
+    }
+
+    private void Update()
+    {
+        if (isSelected)
+        {
+            float angle = rotationSpeed * Time.deltaTime * (rotateClockwise ? 1 : -1);
+
+            if (rotationAxis == RotationAxis.X)
+            {
+                transform.Rotate(Vector3.right, angle);
+            }
+            else if (rotationAxis == RotationAxis.Y)
+            {
+                transform.Rotate(Vector3.up, angle);
+            }
+        }
+    }
+}
